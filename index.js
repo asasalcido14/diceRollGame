f61fb3b53a19b8d59b3038a2b340f1c48d17c7a2
asasalcido14 <asasalcido14@email.arizona.edu>
2021-03-16 20:09:21 -0700

    setting the source for images as a random one using 2 new variables

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 var randomNumber = Math.floor(Math.random() * 6) +1;
+// create a random number variable that will
+var randomDiceImg = "dice" + randomNumber1 + ".png";
+//dice1.png - dice6.png
+var randomImageSource = "images/" + randomDiceImg;
+//images/dice.png - images/dice6.png
