6a583bfe54a83648abe30f7b72d6e7a72178cf94
asasalcido14 <asasalcido14@email.arizona.edu>
2021-03-16 20:37:26 -0700

    added the result alert for h1 to show the outcome of the dice

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var randomImageSource = "images/" + randomDiceImg;
 //images/dice.png - images/dice6.png
 
 var image1 = document.querySelectorAll("img")[0];
-//select all img tag
+
 
 image1.setAttribute("src", randomImageSource);
 
@@ -15,3 +15,15 @@ var randomNumber2 = Math.floor(Math.random() * 6) +1;
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "player 1 wins";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "player 2 wins";
+}
+
+else if (randomNumber1 = randomNumber2) {
+  document.querySelector("h1").innerHTML = "y'all done tied";
+}
